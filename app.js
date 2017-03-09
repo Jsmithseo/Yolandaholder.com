5bfc6c821f89c1adcd83d4fabf4117a7280761a9
Jsmithseo <Jeremysmithseo@gmail.com>
2017-03-09 12:37:04 -0800

    inital commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+	res.render('index',{title: 'Yolanda Holder'});
+});
+
+
+app.get('/mystory', function(req, res){
+	res.render('mystory');
+});
+
+
+app.get('/extreme-walk-4-diabetes', function(req, res){
+	res.render('extreme-walk-4-diabetes');
+});
+
+
+app.get('/contact', function(req, res){
+	res.render('contact');
+});
+
+
+app.get('/speaking-engagements', function(req, res){
+	res.render('speaking-engagements');
+});
+
+
+
+
+
+
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'your@email.com',
+			pass: 'password'
+		}
+	});
+
+	var mailOptions = {
+		from: 'Brad Traversy <techguyinfo@gmail.com>',
+		to: 'support@joomdigi.com',
+		subject: 'Website Submission',
+		text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
+});
+
+app.listen(3000);
+console.log('Server is running on port 3000...');
